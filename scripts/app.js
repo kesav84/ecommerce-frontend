35fef0c4a1e30d14d4ec0e2b6387631dbfce5898
Sri kesav <srikesav84@gmail.com>
2025-04-08 20:40:08 +0530

    Updated HTML files

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,8 @@
 console.log("E-Commerce Website Loaded");
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
